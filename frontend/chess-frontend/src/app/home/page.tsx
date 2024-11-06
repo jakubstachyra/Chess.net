ec9c7ba53477f16c11287a7ad1c15116df5b8e2b
Jakub Stachyra <jakubstachyra@wp.pl>
2024-11-06 14:22:31 +0100

    Add chessboard

--- frontend/chess-frontend/src/app/home/page.tsx
+++ frontend/chess-frontend/src/app/home/page.tsx
@@ -1,10 +1,11 @@
 import RootLayout from '../layout';
 import Chessboard from '../components/chessBoard';
+import HomeLayout from './layout';
 
 export default function Page() {
   return (
-    <RootLayout>
+    <HomeLayout>
       <Chessboard/>
-    </RootLayout>
+    </HomeLayout>
   );
 }
