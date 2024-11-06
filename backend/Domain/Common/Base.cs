3000461f2a2f17bb8e3000211c317acaeeddc59b
Jakub Stachyra <jakubstachyra@wp.pl>
2024-11-06 14:14:21 +0100

    Add dbContext

---
+++ backend/Domain/Common/Base.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Common
+{
+    public class Base
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+    }
+}
