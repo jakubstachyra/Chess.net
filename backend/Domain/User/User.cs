abc1e1b4373091c1a0464c5a55e12c8c0f8fb852
Jakub Stachyra <jakubstachyra@wp.pl>
2024-11-15 20:21:58 +0100

    Add user model

--- backend/Domain/User/User.cs
+++ backend/Domain/User/User.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.User
 {
     public class User: DbContext
     {
-
+        [PrimaryKey]
+        public int ID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public bool IsBanned { get; set; }
     }
 }
