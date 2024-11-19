64c3cd1205439169a11a5292aac2f3d30876b1cc
Jakub Stachyra <jakubstachyra@wp.pl>
2024-11-19 21:02:33 +0100

    Merge wuutj and stachyra work

--- backend/Chess.net/obj/Debug/net8.0/Chess.net.AssemblyInfo.cs
+++ backend/Chess.net/obj/Debug/net8.0/Chess.net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chess.net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4cc399e46e1b90af315a8bdead65440403bfe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72aa49e672dad6ff0789acce8b81558db99b669f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chess.net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chess.net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
