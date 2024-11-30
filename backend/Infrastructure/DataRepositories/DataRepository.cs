9efe8097b483737232975295b51093675dcd3fa7
Jakub Stachyra <jakubstachyra@wp.pl>
2024-11-30 23:37:59 +0100

    Add DataRepository

---
+++ backend/Infrastructure/DataRepositories/DataRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.DataContext;
+using Infrastructure.Interfaces;
+
+namespace Infrastructure.DataRepositories
+{
+    public class DataRepository(DomainDataContext context): IDataRepository
+    {
+        public IGameModeRepository GameModeRepository { get; set; } = new GameModeRepository(context);
+        public IGameRepository GameRepository { get; set; } = new GameRepository(context);
+        public IMoveRepository MoveRepository { get; set; } = new MoveRepository(context);
+        public IRankingRepository RankingRepository { get; set; } = new RankingRepository(context);
+        public IRankingsUserRepository RankingsUserRepository { get; set;} = new RankingsUserRepository(context);
+    }
+}
--- backend/Infrastructure/Interfaces/IMoveRepository.cs
+++ backend/Infrastructure/Interfaces/IMoveRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Common;
 
 namespace Infrastructure.Interfaces
 {
-    internal interface IMoveRepository
-    {
-    }
+    public interface IMoveRepository: IRepository<Move>{}
 }
