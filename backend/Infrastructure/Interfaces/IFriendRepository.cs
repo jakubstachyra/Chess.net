0c496cb0edf6f1e560e99ae43a5c1f4297383faf
Jakub Stachyra <jakubstachyra@wp.pl>
2024-11-30 23:54:08 +0100

    Add friend repository

---
+++ backend/Infrastructure/Interfaces/IFriendRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IFriendRepository: IRepository<Friend> {}
+}
