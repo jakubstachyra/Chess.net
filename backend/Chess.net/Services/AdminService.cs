7e47b015bd62ce5cfc194c83bd912ac31262a162
Jakub Stachyra <jakubstachyra@wp.pl>
2024-12-01 18:29:08 +0100

    Add ban user service

--- backend/Chess.net/Services/AdminService.cs
+++ backend/Chess.net/Services/AdminService.cs
@@ -1,12 +1,27 @@
 ﻿using Chess.net.Services.Interfaces;
 using Domain.Users;
 using Infrastructure.DataContext;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 namespace Chess.net.Services
 {
-    public class AdminService(DomainDataContext context)
+    public class AdminService(UserManager<User> userManager): IAdminService
     {
-        private readonly DomainDataContext _repository = context;
-        
+        private readonly UserManager<User> _usermanager = userManager;
+     
+        public async Task<bool> BanUser(string userId)
+        {
+            var user = await _usermanager.FindByIdAsync(userId);
+
+            if(user == null)
+            {
+                return false;
+            }
+
+            user.IsBanned = true;
+            var result = await _usermanager.UpdateAsync(user);
+            return result.Succeeded;
+        }
     }
 }
