a290685004f883cab6ac1363ed00d9bf8b4cc9cd
Jakub Stachyra <jakubstachyra@wp.pl>
2024-12-09 16:25:15 +0100

    Add TestHelpers class

---
+++ backend/IntegrationTests/AdminControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using Chess.net.Controllers;
+using Chess.net.Services;
+using Domain.Users;
+using Infrastructure.DataContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace IntegrationTests
+{
+    [TestFixture]
+    public class AdminControllerTests
+    {
+    }
+}
