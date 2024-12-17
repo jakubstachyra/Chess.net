d2056454a6a35336a73d069c4855651ea660405e
Jakub Stachyra <jakubstachyra@wp.pl>
2024-12-17 17:14:56 +0100

    Add ApiTests project

---
+++ backend/ApiTests/AccountControllerTests/AccountControllerBaseTests.cs
@@ -0,0 +1,21 @@
+﻿using Logic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ApiTests.AccountControllerTests
+{
+    [TestFixture]
+    public class AccountControllerBaseTests
+    {
+        protected Mock<IAccountService> _accountService;
+        protected AccountController _accountController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _accountService = new Mock<IAccountService>();
+            _accountController = new AccountController(_accountService.Object);
+        }
+
+    }
+}
