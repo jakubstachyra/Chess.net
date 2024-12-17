ff9a4bf7f9e2c93c3b789e754a20510ef92f5c6d
Jakub Stachyra <jakubstachyra@wp.pl>
2024-12-17 17:14:56 +0100

    Add ApiTests project

---
+++ backend/ApiTests/AccountControllerTests/AccountControllerRegisterTests.cs
@@ -0,0 +1,61 @@
+﻿using Domain.AuthModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ApiTests.AccountControllerTests
+{
+    public class AccountControllerRegisterTests : AccountControllerBaseTests
+    {
+
+        [Test]
+        public async Task Register_ShouldReturnOK_WhenRegistrationIsSuccessful()
+        {
+            var registerModel = new RegisterModel
+            {
+                Username = "username",
+                Email = "test@example.com",
+                Password = "password",
+                ConfirmPassword = "password"
+            };
+
+            _accountService.Setup(s => s.RegisterUser(registerModel))
+                               .ReturnsAsync((true, new List<IdentityError>()));
+
+            var result = await _accountController.Register(registerModel);
+
+            var okResult = result as OkObjectResult;
+
+            Assert.IsNotNull(okResult);
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public async Task Register_ReturnsBadRequest_WhenRegistrationFails()
+        {
+
+            var registerModel = new RegisterModel {
+                Username = "username",
+                Email = "test@example.com",
+                Password = "Password123!",
+                ConfirmPassword = "Password123!"
+            };
+            var errors = new List<IdentityError>
+            {
+                new IdentityError { Code = "DuplicateUser", Description = "User already exists" }
+            };
+
+            _accountService.Setup(s => s.RegisterUser(registerModel))
+                               .ReturnsAsync((false, errors));
+
+            var result = await _accountController.Register(registerModel);
+
+            var badRequestResult = result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequestResult);
+            Assert.That(badRequestResult.StatusCode, Is.EqualTo(400));
+
+            var returnedErrors = badRequestResult.Value as IEnumerable<IdentityError>;
+            Assert.That(returnedErrors, Is.Not.Empty);
+        }
+    }
+}
