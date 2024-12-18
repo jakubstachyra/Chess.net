8b068e7a0806e0ef62df18ef06f7464ba0cd4d37
Jakub Stachyra <103006161+jakubstachyra@users.noreply.github.com>
2024-12-18 11:09:56 +0100

    Update RoleInitializer.cs

--- backend/Chess.net/RoleInitializer.cs
+++ backend/Chess.net/RoleInitializer.cs
@@ -2,16 +2,16 @@
 
 public static class RoleInitializer
 {
-    //public static async Task InitializeAsync(RoleManager<IdentityRole> roleManager)
-    //{
-    //    if (!await roleManager.RoleExistsAsync("USER"))
-    //    {
-    //        await roleManager.CreateAsync(new IdentityRole("USER"));
-    //    }
+    public static async Task InitializeAsync(RoleManager<IdentityRole> roleManager)
+    {
+        if (!await roleManager.RoleExistsAsync("USER"))
+        {
+            await roleManager.CreateAsync(new IdentityRole("USER"));
+        }
 
-    //    if (!await roleManager.RoleExistsAsync("ADMIN"))
-    //    {
-    //        await roleManager.CreateAsync(new IdentityRole("ADMIN"));
-    //    }
-    //}
+        if (!await roleManager.RoleExistsAsync("ADMIN"))
+        {
+            await roleManager.CreateAsync(new IdentityRole("ADMIN"));
+        }
+    }
 }
